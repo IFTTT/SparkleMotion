1703d7339fe26d04e4a560079125f55350fa688d
Zhe Lu <lzanita09@gmail.com>
2015-04-25 09:21:03 -0700

    Add ParallaxAnimation demo

---
+++ demo/src/main/java/com/ifttt/jazzhandsdemo/ParallaxViewPagerActivity.java
@@ -0,0 +1,31 @@
+package com.ifttt.jazzhandsdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.ifttt.jazzhands.JazzHands;
+import com.ifttt.jazzhands.JazzHandsViewPager;
+import com.ifttt.jazzhands.animations.Animation;
+import com.ifttt.jazzhands.animations.ParallaxAnimation;
+
+/**
+ * Created by zhelu on 4/25/15.
+ */
+public class ParallaxViewPagerActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.view_pager_layout);
+
+        JazzHandsViewPager jazzHandsViewPager = (JazzHandsViewPager) findViewById(R.id.view_pager);
+        jazzHandsViewPager.setAdapter(new PagerAdapter());
+
+        ParallaxAnimation parallaxAnimation = new ParallaxAnimation(Animation.ALL_PAGES, 2f);
+
+        JazzHands.with(jazzHandsViewPager)
+                .animate(parallaxAnimation)
+                .on(R.id.sunset_img, R.id.sunrise_img);
+    }
+}
