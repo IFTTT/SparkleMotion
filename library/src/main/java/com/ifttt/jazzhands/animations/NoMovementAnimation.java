a60673b920a81034f3cf8b885c12067969adcb39
Zhe Lu <lzanita09@gmail.com>
2015-05-26 10:08:19 -0700

    Add absolute param for NoMovementAnimation

--- library/src/main/java/com/ifttt/jazzhands/animations/NoMovementAnimation.java
+++ library/src/main/java/com/ifttt/jazzhands/animations/NoMovementAnimation.java
@@ -6,16 +6,15 @@ import android.view.View;
  * {@link Animation} subclass that simply takes the {@code offset} from {@link #onAnimate(View, float, float)}
  * and apply that to the target view, so that the view will stay where it is during the page scrolling.
  *
- * This animation by default sets {@code absolute} to true.
  */
 public class NoMovementAnimation extends Animation {
 
-    public NoMovementAnimation(int page) {
-        this(page, page);
+    public NoMovementAnimation(int page, boolean absolute) {
+        this(page, page, absolute);
     }
 
-    public NoMovementAnimation(int start, int end) {
-        super(start, end, true);
+    public NoMovementAnimation(int start, int end, boolean absolute) {
+        super(start, end, absolute);
     }
 
     @Override
