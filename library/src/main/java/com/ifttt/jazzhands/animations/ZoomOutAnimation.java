aca46817bf1bbfc5df43ebbe3d041d9cbd64669a
Zhe Lu <lzanita09@gmail.com>
2015-07-21 07:31:58 -0700

    Add minScale and minAlpha as fields

--- library/src/main/java/com/ifttt/jazzhands/animations/ZoomOutAnimation.java
+++ library/src/main/java/com/ifttt/jazzhands/animations/ZoomOutAnimation.java
@@ -8,21 +8,33 @@ import com.ifttt.jazzhands.Animation;
  * Subclass of {@link Animation} that runs a more complex animation, which scales the view down and fade out
  * the view to a certain value as the ViewPager scrolls, and return back to original values when the page is
  * scrolled.
- *
+ * <p/>
  * Implementation details are from http://developer.android.com/training/animation/screen-slide.html#pagetransformer.
  */
 public class ZoomOutAnimation extends Animation {
-    private static final float MIN_SCALE = 0.85f;
-    private static final float MIN_ALPHA = 0.5f;
+    private static final float DEFAULT_MIN_SCALE = 0.85f;
+    private static final float DEFAULT_MIN_ALPHA = 0.5f;
+
+    private final float mMinScale;
+    private final float mMinAlpha;
 
     /**
      * Base constructor of the class, accepting common information about the animation to this instance.
      *
      * @param start    Page index that this animation should start.
      * @param end      Page index that this animation should stop.
+     * @param minScale Minimum scale X and Y of the View when animating.
+     * @param minAlpha Minimum alpha of the View when animating.
      */
-    public ZoomOutAnimation(int start, int end) {
+    public ZoomOutAnimation(int start, int end, float minScale, float minAlpha) {
         super(start, end, false);
+
+        mMinAlpha = minAlpha;
+        mMinScale = minScale;
+    }
+
+    public ZoomOutAnimation(int start, int end) {
+        this(start, end, DEFAULT_MIN_SCALE, DEFAULT_MIN_ALPHA);
     }
 
     public ZoomOutAnimation(int page) {
@@ -44,7 +56,7 @@ public class ZoomOutAnimation extends Animation {
         int pageWidth = v.getWidth();
         int pageHeight = v.getHeight();
 
-        float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(fraction));
+        float scaleFactor = Math.max(mMinScale, 1 - Math.abs(fraction));
         float vertMargin = pageHeight * (1 - scaleFactor) / 2;
         float horzMargin = pageWidth * (1 - scaleFactor) / 2;
 
@@ -59,9 +71,9 @@ public class ZoomOutAnimation extends Animation {
         v.setScaleY(scaleFactor);
 
         // Fade the page relative to its size.
-        v.setAlpha(MIN_ALPHA +
-                (scaleFactor - MIN_SCALE) /
-                        (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+        v.setAlpha(mMinAlpha +
+                (scaleFactor - mMinScale) /
+                        (1 - mMinScale) * (1 - mMinAlpha));
     }
 
 
